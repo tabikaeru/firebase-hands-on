69ae9296e7de8453133cd96b0c5144f89817fb66
tabikaeru <moriokayuixxx@gmail.com>
2020-08-15 14:12:10 +0900

    feat:helloworld

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,6 @@
 import * as functions from 'firebase-functions'
-import * as admin from 'firebase-admin'
 
-require('dotenv').config({ path: __dirname + '/../.env' })
-
-admin.initializeApp({
-  credential: admin.credential.cert(__dirname + '/../google-service-account.json'),
-  databaseURL: process.env.DATABASE_URL
-})
 export const helloWorld = functions.https.onRequest((request, response) => {
-  const db = admin.firestore()
-  const batch = db.batch()
-  batch.set(db.collection('helloWorld').doc(), { hello: 'world' })
+  functions.logger.info('Hello logs!', { structuredData: true })
+  response.send('Hello from Firebase!')
 })
