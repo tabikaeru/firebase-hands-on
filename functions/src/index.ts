f22c88cad3668361cf0f81295c342b619335ed8d
tabikaeru <moriokayuixxx@gmail.com>
2020-08-19 13:44:38 +0900

    feeat:functions_with_args

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,13 @@
 import * as functions from 'firebase-functions'
 
-//Lesson 1. firestoreへの書きこみ: 認証の初期化
+//Setting Up. firestoreへの書きこみ: 認証の初期化
 // import * as admin from 'firebase-admin'
 
 // const serviceAccount = '../../firebase-hands-on/google-service-account.json'
 
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: 'YOUR_DATABASE_URL'//DBのURLを書き換え
+//   databaseURL: 'YOUR_DATABASE_URL' //DBのURLを書き換え
 // })
 
 export const helloWorld = functions.https.onRequest((request, response) => {
@@ -17,8 +17,18 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 
 //Lesson 1. firestoreへの書きこみ: firestoreへ書き込むonCall関数の定義
 // export const helloWorldOnCall = functions.https.onCall(async (data, context) => {
-//   const db = admin.firestore()
-//   const batch = db.batch()
-/*ここにfirestoreへの書き込みコードを書く*/
-//   batch.commit()
+// const db = admin.firestore()
+
+/*TASK: ここにfirestoreへの書き込みコードを書いてみよう！*/
+
+// })
+
+//Lesson 2. 引数をつけてfirestoreへの書きこみ: firestoreへ書き込むonCall関数の定義
+// export const helloWorldOnCallWithArg = functions.https.onCall(async (data, context) => {
+// const db = admin.firestore()
+
+/*TASK:ここに引数を読み込むコードを書いてみよう！*/
+
+/*TASK: ここに引数を使ったfirestoreへの書き込みコードを書いてみよう！*/
+
 // })
